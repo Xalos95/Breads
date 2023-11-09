a8f4481c9b92b22e5817395aeb567745f3e48e6d
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-11-08 20:52:46 -0800

    update 2

---
+++ views/layouts/Default.jsx
@@ -0,0 +1,19 @@
+const React = require('react')
+
+function Default(html) {
+    return (
+      <html>
+          <head>
+              <title>Default</title>
+              <body>
+                  <h1>HTML Rendered!</h1>
+                  <div className="container">
+                      {html.children}
+                  </div>
+              </body>
+          </head>
+      </html>
+    )
+}
+  
+module.exports = Default
